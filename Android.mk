5b2ab966806e14b17a36202e04253a87e6a177a3
qi.duan <qi.duan@amlogic.com>
2012-05-17 17:05:29 +0800

    add usbtestpm interface for mx, two type

--- Android.mk
+++ Android.mk
@@ -17,5 +17,32 @@ LOCAL_CFLAGS := $(L_CFLAGS)
 LOCAL_SRC_FILES := logwrapper.c usbtestpm.cpp
 LOCAL_C_INCLUDES := $(INCLUDES)
 include $(BUILD_EXECUTABLE)
+
+include $(CLEAR_VARS)
+LOCAL_MODULE := usbpower_mx_iddq
+LOCAL_MODULE_TAGS := optional
+LOCAL_SHARED_LIBRARIES := libc libcutils
+LOCAL_CFLAGS := $(L_CFLAGS)
+LOCAL_SRC_FILES := usbpower_mx_iddq.cpp
+LOCAL_C_INCLUDES := $(INCLUDES)
+include $(BUILD_EXECUTABLE)
+
+include $(CLEAR_VARS)
+LOCAL_MODULE := usbtestpm_mx
+LOCAL_MODULE_TAGS := optional
+LOCAL_SHARED_LIBRARIES := libc libcutils
+LOCAL_CFLAGS := $(L_CFLAGS)
+LOCAL_SRC_FILES := logwrapper.c usbtestpm_mx.cpp
+LOCAL_C_INCLUDES := $(INCLUDES)
+include $(BUILD_EXECUTABLE)
+
+include $(CLEAR_VARS)
+LOCAL_MODULE := usbtestpm_mx_iddq
+LOCAL_MODULE_TAGS := optional
+LOCAL_SHARED_LIBRARIES := libc libcutils
+LOCAL_CFLAGS := $(L_CFLAGS)
+LOCAL_SRC_FILES := logwrapper.c usbtestpm_mx_iddq.cpp
+LOCAL_C_INCLUDES := $(INCLUDES)
+include $(BUILD_EXECUTABLE)
 endif
 
